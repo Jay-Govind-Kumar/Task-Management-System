90301e4f3908a9bac229ab634967e2a3f46bf76b
Jay-Govind <govind.iq@gmail.com>
2025-05-10 09:03:44 +0530

    feat(routes): add healthcheck endpoint
    Implemented a basic healthcheck route to verify server status.
    This can be used by monitoring tools or load balancers to confirm the app is running.

---
+++ src/controllers/healthcheck.controllers.js
@@ -0,0 +1,15 @@
+import { ApiResponse } from "../utils/api-response.js";
+
+const healthCheck = async (req, res) => {
+  try {
+    res
+      .status(200)
+      .json(new ApiResponse(200, { message: "Server is running" }));
+  } catch (error) {
+    res
+      .status(500)
+      .json(new ApiResponse(500, { message: "Internal Server Error" }));
+  }
+};
+
+export { healthCheck };
