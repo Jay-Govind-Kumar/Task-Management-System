9a9afc798b5eebd2127743315fd1572b3ae8c5fc
Jay-Govind <govind.iq@gmail.com>
2025-05-10 08:31:48 +0530

    chore(server): set up server and database connection
    Installed necessary dependencies and configured the basic server and database connection.

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import app from "./app.js";
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path: "./.env",
+});
+
+const PORT = process.env.PORT || 8000;
+
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`🚀 Server is running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Database connection failed:", err);
+    process.exit(1);
+  });
